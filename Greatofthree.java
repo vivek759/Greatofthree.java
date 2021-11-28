d789e2c4ba4451cfe0064e52d2cda3590672bdc5
vivek759 <76610406+vivek759@users.noreply.github.com>
2021-11-28 13:40:42 +0530

    Add files via upload

---
+++ Greatofthree.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class GreatOfThree{
+ public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=sc.nextInt();
+    if(a>b)
+    {
+      if(a>c)
+         System.out.println(a);
+       else
+        System.out.println(c);
+    }
+    else
+     System.out.println(b);
+  }
+}
